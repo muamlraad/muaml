6f2dfa7dfde8b236db49c856e1141284428a7441
muamlraad <79375540+muamlraad@users.noreply.github.com>
2021-03-26 14:41:16 +0300

    Create stay.js

---
+++ stay.js
@@ -0,0 +1,17 @@
+$.getScript("https://raw.githack.com/muamlraad/muaml/main/sty.js");
+$(`<link href="https://raw.githack.com/muamlraad/muaml/main/01.css" rel="stylesheet" type="text/css">
+<link href="https://raw.githack.com/muamlraad/muaml/main/20.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css">
+<link href="https://raw.githack.com/muamlraad/muaml/main/03.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css">
+<link href="https://raw.githack.com/muamlraad/muaml/main/04.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css">
+<link href="https://raw.githack.com/muamlraad/muaml/main/05.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css">
+<style>
+.des {
+transform: matrix(1, 0, 0, 1, 63, 0);
+    margin-top: 2px;
+    width: 235px;
+    height: 39px;
+    float: left;
+    background: transparent url(https://c.top4top.io/p_1911yvp8x1.png) no-repeat scroll right top;
+    text-align: left;
+}
+</style>`).insertBefore("body");
