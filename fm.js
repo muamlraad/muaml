16524ba09ee1b6318004fb37429dd55c6e4f3a43
muamlraad <79375540+muamlraad@users.noreply.github.com>
2022-08-22 21:28:10 +0300

    Create fm.js

---
+++ fm.js
@@ -0,0 +1,11 @@
+$('.tc span[class="fa fl"]').addClass('fa-check');var nopm=!0,nonot=!0;$("##settings").append(`<label title="لحظات "href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show()"data-toggle="tab"data-target="#mic"class="asim ae fa label label-primary"><i class="fa fa-fire"aria-hidden="true"></i></label>`);$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(`<div id="mic"style="height: 100%;width:100%;"class="break light tab-pane border"><center></div><center></div>`),$("div#tlogins button.btn.btn-primary").click(function(){var myVar=setInterval(function(){var usmsgw=$(".pmsgc").length;if(usmsgw>0){$(`<textarea class="as2020" style="height:146px;overflow:hidden">
+⠀ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀ 
+♔ ★  ║ 彡  ♫ • ❥ ✿  ⌯ ✘ ●┊♡♰❰ ❱ ᵛ͢ᵎᵖ  ₎™ 🦅🌹💀☠️👑🌝✨💥🔥🍂☂ ❥ ‏˛⁽ ツ‏⠀ ᵀᴹ )"̯ء 
+ڪـٰٖـٖ ـٰـٰٰـٰٰة ـٰٰٰٰ۪ٖ۫ـٰہـةﺔﺔ ،آ‘إْــِْـﮧٰٖـــــِْﮧٰٖــ ۽ۧ۽ۧ ــٰٰـٰـٰٰ ـٰٰـٰـٰٰـﮯ̷͡ـــ ـــٰۦـٰٖـٰـٰہٰٖءـٰـٰۦــ ــ♯ــٰـٰـٰۦـٰٰٰٖٖ
+qતۦۦ᎗ۦۦ᎗බລܭɹ̇൧ρᘓ۶ᒧხbl͠ 
+</textarea></div>`).appendTo("#mic")
+clearInterval(myVar);}else{console.log(usmsgw)}},2000);})
+$('textarea#tbox').bind("contextmenu", function(e){
+e.preventDefault();
+}, false);
+$('textarea#tbox').on("paste",function(e){e.preventDefault();});
